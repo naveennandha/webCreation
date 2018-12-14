3430e1e76969793300da6d9a6ad56294f9e70c63
naveennandha <naveennnk1996@gmail.com>
2018-12-14 22:36:57 +0530

    14/12/2018

--- js/custom.js
+++ js/custom.js
@@ -5,3 +5,32 @@
 $(function () {
     new WOW().init();
 });
+
+
+/*==============
+    WORK
+===============*/
+$(function () {
+    $("#work").magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image', // it will open all images
+        gallery: {
+            enabled: true // each and every image will be open as a gallery view.
+        },
+    });
+});
+
+
+/*==============
+    TEAM
+===============*/
+
+$(function () {
+    $("#team-members").owlCarousel({
+        items: 3,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true
+    });
+});
